c21267cbb31f9e4dce3de5bfa869c2ef6c294ad6
Pedro Henrique <pehdfms@gmail.com>
2022-05-28 20:48:35 -0300

    improve search and pagination behaviour

--- src/components/APODList.vue
+++ src/components/APODList.vue
@@ -2,17 +2,34 @@
     <v-toolbar>
       <v-text-field
           hide-details
-          prepend-icon="mdi-magnify"
           single-line
-          v-model="titleQuery"
+          v-model="queuedQuery"
+          placeholder="Search"
       >
       </v-text-field>
+
+      <v-btn icon
+         @click="search"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon
+         @click="reset"
+      >
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn>
+
     </v-toolbar>
+
+    <v-pagination :length="results?.page?.totalPages" v-model="page"></v-pagination>
+
     <div class="card-list">
       <div v-for="planet in results?._embedded?.planets" :key="planet.id">
         <APODCard :apod="planet"></APODCard>
       </div>
     </div>
+
     <v-pagination :length="results?.page?.totalPages" v-model="page"></v-pagination>
 </template>
 
@@ -21,11 +38,23 @@ import APODCard from "@/components/APODCard";
 export default {
   name: "APODList",
   components: {APODCard},
+  methods: {
+    search: function search() {
+      titleQuery.value = queuedQuery.value
+      page.value = 1
+    },
+    reset: function reset() {
+      titleQuery.value = ""
+      queuedQuery.value = ""
+      page.value = 1
+    }
+  },
   data() {
     return {
       results: results,
       page: page,
-      titleQuery: titleQuery
+      titleQuery: titleQuery,
+      queuedQuery: queuedQuery
     }
   }
 }
@@ -33,18 +62,14 @@ export default {
 import { ref, watchEffect } from 'vue'
 
 const API_ROOT = 'http://localhost:8080/'
-const API_URL = API_ROOT + 'planets?size=10'
 
 const results = ref({})
 const titleQuery = ref("")
+const queuedQuery = ref("")
 const page = ref(1)
 
 watchEffect(async () => {
-  let url = API_URL
-
-  if (titleQuery.value) {
-    url = API_ROOT + "planets/search/findByTitle?title=" + titleQuery.value + "&size=10"
-  }
+  let url = API_ROOT + "planets/search/findByTitle?title=" + titleQuery.value
 
   url += '&page=' + (page.value - 1).toString()
   console.log(url)
